91245133ad793d3ad041c01c735f0b5219b1e3e6
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-05-23 12:53:19 -0600

    registered user sees list of email addresses without links on landing page

--- spec/features/authentication/landing_page_spec.rb
+++ spec/features/authentication/landing_page_spec.rb
@@ -10,7 +10,20 @@ RSpec.describe "Authentication" do
       visit root_path
       User.all.each do |user|
         expect(page).to have_no_content(user.email)
-        expect(page).to have_no_content(user.name)
+      end
+    end
+
+    it "only email addresses appear for registered users, without links" do
+      visit root_path
+      click_on("Log In")
+      fill_in :email, with: "#{@user_1.email}"
+      fill_in :password, with: "#{@user_1.password}"
+      click_on "Log In"
+      click_on "Home"
+
+      User.all.each do |user|
+        expect(page).to have_content(user.email)
+        expect(page).to_not have_link(user.email)
       end
     end
   end
